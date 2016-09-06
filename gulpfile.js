7c8099abb46334df8543efd6e727840cd9478811
Ian Segers <iansegers@gmail.com>
2016-09-06 23:18:11 +0200

    Content Responsiveness

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+/*******************************
+	Main build tasks
+ *******************************/
+gulp.task('watch', ['serve', 'watch:sass', 'watch:html']);
+gulp.task('publish', ['sass-publish']);
+
+/*******************************
+	Development build tasks
+ *******************************/
 gulp.task('serve', ['sass'], function() {
 	browserSync.init({
 		server: "./client"
@@ -9,18 +18,25 @@ gulp.task('serve', ['sass'], function() {
 });
 
 gulp.task('watch:sass', function () {
-	gulp.watch("client/assets/sass/*.scss", ['sass']);
+	gulp.watch("client/assets/sass/*.scss", ['sass-watch']);
 });
 
 gulp.task('watch:html', function () {
 	gulp.watch("client/**/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('sass', function() {
+gulp.task('sass-watch', function() {
 	return gulp.src("client/assets/sass/**/*.scss")
 		.pipe(sass())
 		.pipe(gulp.dest("./client/assets/css"))
 		.pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['serve', 'watch:sass', 'watch:html']);
+/*******************************
+	Publish build tasks
+ *******************************/
+gulp.task('sass-publish', function() {
+	return gulp.src("client/assets/sass/**/*.scss")
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(gulp.dest("./client/assets/css"));
+});
